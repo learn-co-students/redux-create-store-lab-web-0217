4a2184393b9d1d0dc52c5efc9ba53ddf85cbb0af
github username <github email address>
2017-04-11 17:14:03 -0400

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,6 +1,19 @@
 export default function createStore(reducer) {
-  // add your code here
+  let state
+
+  const getState =() => {
+    return state
+  }
+
+  const dispatch = (action) => {
+    state = reducer(state, action)
+    render()
+  }
+  
+  dispatch({type: "initialize"})
+  return {getState, dispatch}
 }
+
 
 function render() {
   const container = document.getElementById('container');
